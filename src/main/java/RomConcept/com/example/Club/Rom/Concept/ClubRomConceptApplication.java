7dd39b7c172356c5345a619cf938fa9508129df6
Silas Melo <silasmelo12@gmail.com>
2026-04-09 09:32:34 -0300

    feat: setup inicial do projeto

---
+++ src/main/java/RomConcept/com/example/Club/Rom/Concept/ClubRomConceptApplication.java
@@ -0,0 +1,12 @@
+package RomConcept.com.example.Club.Rom.Concept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubRomConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubRomConceptApplication.class, args);
+	}
+}
---
+++ src/test/java/RomConcept/com/example/Club/Rom/Concept/ClubRomConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package RomConcept.com.example.Club.Rom.Concept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubRomConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
