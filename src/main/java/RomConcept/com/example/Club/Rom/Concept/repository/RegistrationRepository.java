2e879815b0ce243359b2421fa85dc4f606151286
Silas Melo <silasmelo12@gmail.com>
2026-04-09 11:53:25 -0300

    feat: add repositories for User, Event, and Registration models

---
+++ src/main/java/RomConcept/com/example/Club/Rom/Concept/repository/RegistrationRepository.java
@@ -0,0 +1,9 @@
+package RomConcept.com.example.Club.Rom.Concept.repository;
+
+import RomConcept.com.example.Club.Rom.Concept.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<User, Integer> {
+}
